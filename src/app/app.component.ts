c7cef54180103f3ff67eb5713ca58e7d851b5df4
manojdsgowda <manojdsgowda@gmail.com>
2019-11-27 18:17:05 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit  {
   test:string;
 
   ngOnInit() {
-    this.list = [{title: 'Physics',},
+    this.list = [{title: 'Physics'},
                  {title: 'Chemistry'},
                  {title: 'Maths'},
                  {title: 'Biology'},
@@ -25,9 +25,41 @@ export class AppComponent implements OnInit  {
       onClickMe() {
         this.clickMessage1 = 'Student Name is '+ this.text1;
     //this.clickMessage = this.list.filter(item => item.checked);
-
     this.clickMessage =JSON.stringify(this.list.filter(item =>
     item.checked));
   }
 
+}
+
+
+
+import { Component } from '@angular/core';
+import { FormGroup, FormArray, FormControl, FormBuilder} from '@angular/forms';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  emailFormArray: Array<any> = [];
+  test: Array<any> = [];
+  categories = [ 
+    {name :"email1", id: 1},
+    {name :"email2", id: 2},
+    {name :"email3", id: 3},
+    {name :"email4", id: 4}
+  ];
+
+  onChange(email:string, isChecked: boolean) {
+      if(isChecked) {
+        this.emailFormArray.push(email);
+      } else {
+        let index = this.emailFormArray.indexOf(email);
+        this.emailFormArray.splice(index,1);
+      }
+  }
+
+  onClickMe() {
+     this.test= this.emailFormArray;
+  }
 }
